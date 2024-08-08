72bf400a17b357f601a36b245850e44e67ac1422
Ayush Morbar <108860844+ayushmorbar@users.noreply.github.com>
2024-08-08 19:18:31 +0000

    chore: Update next.config.mjs for static exports and strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
+    basePath: "/PanTrack",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
   };
